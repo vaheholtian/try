12bf9d97c8e4cea779d7aa203642f96e4e385594
vaheholtian <vahe.holtian@gmail.com>
2021-11-19 17:51:49 -0500

    Update vaheplugin.php

--- plugins/vaheplugin.php
+++ plugins/vaheplugin.php
@@ -12,6 +12,50 @@ Version: 1
 Author URI: http://localhost/
 */
 
+
+function cptui_register_my_cpts_movies() {
+
+	/**
+	 * Post Type: Movies.
+	 */
+
+	$labels = [
+		"name" => __( "Movies", "my-simple-theme" ),
+		"singular_name" => __( "Movie", "my-simple-theme" ),
+	];
+
+	$args = [
+		"label" => __( "Movies", "my-simple-theme" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => [ "slug" => "movies", "with_front" => true ],
+		"query_var" => true,
+		"menu_icon" => "dashicons-format-video",
+		"supports" => [ "title", "editor", "thumbnail" ],
+		"taxonomies" => [ "category", "post_tag", "artists" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "movies", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_movies' );
+
+
 function create_client_tax() {
     register_taxonomy( 
             'client_tag', //your tags taxonomy
